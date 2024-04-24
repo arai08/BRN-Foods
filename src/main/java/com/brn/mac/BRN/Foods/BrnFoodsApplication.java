b85cb3aec2645ae8b8fd82a3e63d701420777848
Rehaan Arai <rehaanarai@Rehaans-MacBook-Pro.local>
2024-04-24 17:46:39 -0400

    project setup

---
+++ src/main/java/com/brn/mac/BRN/Foods/BrnFoodsApplication.java
@@ -0,0 +1,13 @@
+package com.brn.mac.BRN.Foods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrnFoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrnFoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brn/mac/BRN/Foods/BrnFoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brn.mac.BRN.Foods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrnFoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
